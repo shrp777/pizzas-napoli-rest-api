b04cf7943c03249a4cbfc2fa3b386e8915f133f2
Shrp <alex@sherpa.one>
2025-01-08 15:54:30 +0100

    suppression de l'implémentation de l'API

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,9 @@
 import { Hono } from "hono";
-import pizzasRoutes from "./routes/pizzas";
 
 const app = new Hono();
 
 app.get("/", (c) => {
   return c.json({ message: "Pizzas Napoli API" });
 });
-
-app.route("/pizzas", pizzasRoutes);
 
 export default app;
--- src/types.ts
+++
@@ -1,6 +0,0 @@
-export type Pizza = {
-  id: number;
-  name: string;
-  ingredients: string[];
-  price: number;
-};
