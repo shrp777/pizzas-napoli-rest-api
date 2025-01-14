8c45b7e88d4e8c34ce7f6996474a0000ab4f112a
Shrp <alex@sherpa.one>
2025-01-14 14:37:41 +0100

    add get pizza by id + get pizzas collection filtered by base

--- src/routes/pizzasEndpoint.ts
+++ src/routes/pizzasEndpoint.ts
@@ -3,7 +3,24 @@ import { pizzas } from "../data";
 
 export const pizzasEndpoint = new Hono();
 
+pizzasEndpoint.get("/:id", (c) => {
+  const id = c.req.param().id;
+  return c.json({ pizza: { id: Number.parseInt(id) } }); // à remplacer par la vraie pizza issue du tableau pizzas
+});
+
 pizzasEndpoint.get("/", (c) => {
+  const query = c.req.query();
+
+  console.log(query); //retourne un objet avec pour chaque variable dans l'URL, une clé dans l'objet du même nom
+
+  const basePizza = query.base; //accès direct à la clé base (si elle existe)
+
+  console.log(basePizza);
+
+  const { base } = c.req.query(); //création dynamique d'une variable base, par destructuration de l'objet retourné par c.req.query()
+
+  console.log(base);
+
   //return c.json({ pizzas: [] });
   return c.json({ pizzas });
 });
