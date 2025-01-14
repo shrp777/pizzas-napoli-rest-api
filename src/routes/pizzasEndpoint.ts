052a1774551385542dcc20cf5b184ce994d671d4
Lore Goethals <lore.goethals2@etu.univ-lorraine.fr>
2025-01-14 20:23:00 +0100

    adding router for /pizzas GET

--- src/routes/pizzasEndpoint.ts
+++ src/routes/pizzasEndpoint.ts
@@ -1,27 +1,35 @@
 import { Hono } from "hono";
-import { pizzas } from "../data";
+import { pizzasCollection } from "../data";
 
 const pizzasEndpoint = new Hono();
 
 pizzasEndpoint.get("/:id", (c) => {
-  const id = c.req.param().id;
-  return c.json({ pizza: { id: Number.parseInt(id) } }); // à remplacer par la vraie pizza issue du tableau pizzas
+  const id = c.req.param('id');
+  const pizza = pizzasCollection.find((element) => element.id === parseInt(id));
+  return c.json({ pizza });
+  //return c.json({ pizza: { id: Number.parseInt(id) } }); // à remplacer par la vraie pizza issue du tableau pizzasCollection
 });
 
 pizzasEndpoint.get("/", (c) => {
   const query = c.req.query();
-
   console.log(query); //retourne un objet avec pour chaque variable dans l'URL, une clé dans l'objet du même nom
 
   const basePizza = query.base; //accès direct à la clé base (si elle existe)
-
   console.log(basePizza);
 
-  const { base } = c.req.query(); //création dynamique d'une variable base, par destructuration de l'objet retourné par c.req.query()
-
+  const { base, price } = c.req.query(); //création dynamique d'une variable base, par destructuration de l'objet retourné par c.req.query()
   console.log(base);
 
-  //return c.json({ pizzas: [] });
+  let pizzas = pizzasCollection;
+
+  if(base){
+     pizzas = pizzas.filter(p=>p.base===base);
+  }
+
+  if(price){
+    pizzas = pizzas.filter(p=>p.price===Number(price));
+  }
+
   return c.json({ pizzas });
 });
 
