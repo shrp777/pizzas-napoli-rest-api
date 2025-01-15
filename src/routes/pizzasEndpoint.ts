cab1c8d7497073f11e7f76bb4c6f30f42517a15f
Shrp <alex@sherpa.one>
2025-01-15 19:32:25 +0100

    Update pizzasEndpoint.ts
    Suppression des console.log et du code commenté

--- src/routes/pizzasEndpoint.ts
+++ src/routes/pizzasEndpoint.ts
@@ -7,19 +7,15 @@ pizzasEndpoint.get("/:id", (c) => {
   const id = c.req.param('id');
   const pizza = pizzasCollection.find((element) => element.id === parseInt(id));
   return c.json({ pizza });
-  //return c.json({ pizza: { id: Number.parseInt(id) } }); // à remplacer par la vraie pizza issue du tableau pizzasCollection
 });
 
 pizzasEndpoint.get("/", (c) => {
   const query = c.req.query();
-  console.log(query); //retourne un objet avec pour chaque variable dans l'URL, une clé dans l'objet du même nom
-
+  
   const basePizza = query.base; //accès direct à la clé base (si elle existe)
-  console.log(basePizza);
-
+  
   const { base, price } = c.req.query(); //création dynamique d'une variable base, par destructuration de l'objet retourné par c.req.query()
-  console.log(base);
-
+  
   let pizzas = pizzasCollection;
 
   if(base){
