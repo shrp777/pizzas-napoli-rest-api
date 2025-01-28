001bc6fe6e8b01995a7d373d1d7bea03e31466b5
Shrp <alex@sherpa.one>
2025-01-28 11:26:11 +0100

    commentaires et améliorations de la route pizzas/{id}

--- src/routes/pizzasEndpoint.ts
+++ src/routes/pizzasEndpoint.ts
@@ -4,26 +4,29 @@ import { pizzasCollection } from "../data";
 const pizzasEndpoint = new Hono();
 
 pizzasEndpoint.get("/:id", (c) => {
-  const id = c.req.param('id');
-  const pizza = pizzasCollection.find((element) => element.id === parseInt(id));
+  const id: number = parseInt(c.req.param("id"));
+  //récupère l'id dans le path de l'URI
+  //on convertit l'id en nombre
+  const pizza = pizzasCollection.find((p) => p.id === id);
+  //on cherche la pizza dont l'id est égal au paramètre id récupéré
   return c.json({ pizza });
 });
 
 pizzasEndpoint.get("/", (c) => {
   const query = c.req.query();
-  
+
   const basePizza = query.base; //accès direct à la clé base (si elle existe)
-  
+
   const { base, price } = c.req.query(); //création dynamique d'une variable base, par destructuration de l'objet retourné par c.req.query()
-  
+
   let pizzas = pizzasCollection;
 
-  if(base){
-     pizzas = pizzas.filter(p=>p.base===base);
+  if (base) {
+    pizzas = pizzas.filter((p) => p.base === base);
   }
 
-  if(price){
-    pizzas = pizzas.filter(p=>p.price===Number(price));
+  if (price) {
+    pizzas = pizzas.filter((p) => p.price === Number(price));
   }
 
   return c.json({ pizzas });
