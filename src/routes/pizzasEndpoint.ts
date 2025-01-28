158bbffa75cf17d7c0b686dcaf1788fde6aa66e4
Shrp <alex@sherpa.one>
2025-01-28 11:52:32 +0100

    gestion des id incorrects dans /pizzas/{id}

--- src/routes/pizzasEndpoint.ts
+++ src/routes/pizzasEndpoint.ts
@@ -1,14 +1,23 @@
 import { Hono } from "hono";
 import { pizzasCollection } from "../data";
+import { HTTPException } from "hono/http-exception";
 
 const pizzasEndpoint = new Hono();
 
 pizzasEndpoint.get("/:id", (c) => {
   const id: number = parseInt(c.req.param("id"));
+
+  //si la conversion de l'id ne permet pas d'obtenir un nombre (undefined, null ou NaN)
+  if (!id) {
+    //on déclenche une exception qui sera convertie en réponse HTTP avec le code 400
+    throw new HTTPException(400);
+  }
+
   //récupère l'id dans le path de l'URI
   //on convertit l'id en nombre
   const pizza = pizzasCollection.find((p) => p.id === id);
   //on cherche la pizza dont l'id est égal au paramètre id récupéré
+
   return c.json({ pizza });
 });
 
