18cb2ab7a2fec11c0b260bb5d151110af6020902
Shrp <alex@sherpa.one>
2025-02-03 15:47:03 +0100

    add pizzas endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import { Hono } from "hono";
 import { connectDatabase, createSchema, seedData } from "./db/database";
+import pizzasEndpoint from "./routes/pizzasEndpoint";
 
 const app = new Hono();
 
 app.get("/", (c) => {
   return c.json({ message: "Pizzas Napoli API" });
 });
+
+app.route("/pizzas", pizzasEndpoint);
 
 export default app;
